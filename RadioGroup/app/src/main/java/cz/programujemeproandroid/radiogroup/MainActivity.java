269eb3a0bf84ac75b9369c1a358bb9bee3826428
100xAndroid <128327670+100xAndroid@users.noreply.github.com>
2023-03-26 19:02:21 +0200

    Prvni verze zdrojovych souboru

---
+++ RadioGroup/app/src/main/java/cz/programujemeproandroid/radiogroup/MainActivity.java
@@ -0,0 +1,15 @@
+package cz.programujemeproandroid.radiogroup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cz.programujemeproandroid.radiogroup.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
